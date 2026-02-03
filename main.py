e100d99ad81638559aec25ff2ca5aa4b5c68db27
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-02-02 22:52:18 -0300

    Modelo Pré-processamento

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import os
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
 pd.set_option("display.max_rows", 200)
 pd.set_option("display.max_columns", 1000)
 pd.set_option("display.width", 1000)
@@ -112,13 +115,18 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # 4. Instanciar e treinar o modelo
-modelo = GaussianNB()
-modelo.fit(X_train, y_train)
+pipeline = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", GaussianNB())
+])
 
-# 5. Previsões
-y_pred = modelo.predict(X_test)
+# 5. Treinar
+pipeline.fit(X_train, y_train)
 
-# 6. Avaliação
+# 6. Prever
+y_pred = pipeline.predict(X_test)
+
+# 7. Avaliação
 print("Acurácia:", accuracy_score(y_test, y_pred))
 print("\nRelatório de Classificação:")
 print(classification_report(y_test, y_pred))
